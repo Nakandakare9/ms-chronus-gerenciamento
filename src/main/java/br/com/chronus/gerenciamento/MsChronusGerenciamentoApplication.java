1cde6a3d387b3151ba9faaf3138b14da882cf5a8
Du Nakandakare <nakandakare9@gmail.com>
2025-07-03 14:02:51 -0300

    Implementacao gerenciamento

---
+++ src/main/java/br/com/chronus/gerenciamento/MsChronusGerenciamentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.chronus.gerenciamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsChronusGerenciamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsChronusGerenciamentoApplication.class, args);
+	}
+
+}
